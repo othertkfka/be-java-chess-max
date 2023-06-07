039298e2df330bbb6aa0783a86f9af7da7be132a
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-06-07 15:54:52 +0900

    feat: Pawn을 Piece로 변경
    Piece로 모든 기물을 생성할 수 있도록 변경
    모든 기물 생성 테스트 코드 작성

---
+++ src/main/java/pieces/Piece.java
@@ -0,0 +1,83 @@
+package pieces;
+
+public class Piece {
+    public static final String BLACK_COLOR = "black";
+    public static final String WHITE_COLOR = "white";
+
+    public static final String BLACK_PAWN_REPRESENTATION = "P";
+    public static final String WHITE_PAWN_REPRESENTATION = "p";
+    public static final String BLACK_KNIGHT_REPRESENTATION = "N";
+    public static final String WHITE_KNIGHT_REPRESENTATION = "n";
+    public static final String BLACK_ROOK_REPRESENTATION = "R";
+    public static final String WHITE_ROOK_REPRESENTATION = "r";
+    public static final String BLACK_BISHOP_REPRESENTATION = "B";
+    public static final String WHITE_BISHOP_REPRESENTATION = "b";
+    public static final String BLACK_QUEEN_REPRESENTATION = "Q";
+    public static final String WHITE_QUEEN_REPRESENTATION = "q";
+    public static final String BLACK_KING_REPRESENTATION = "K";
+    public static final String WHITE_KING_REPRESENTATION = "k";
+
+    private final String color;
+    private final String representation;
+
+    private Piece(String color, String representation) {
+        this.color = color;
+        this.representation = representation;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(BLACK_COLOR, BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(WHITE_COLOR, WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(BLACK_COLOR, BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(WHITE_COLOR, WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(BLACK_COLOR, BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(WHITE_COLOR, WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BLACK_COLOR, BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(WHITE_COLOR, WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(BLACK_COLOR, BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(WHITE_COLOR, WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(BLACK_COLOR, BLACK_KING_REPRESENTATION);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
+}
--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -8,24 +8,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 또는 검은색으로 폰이 생성된다.")
+    @DisplayName("흰색 또는 검은색으로 기물이 생성된다.")
     public void create() {
-        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
-    public void verifyPawn(final String color, final String representation) {
-        Pawn pawn = new Pawn(color, representation);
-        assertThat(pawn.getColor()).isEqualTo(color);
-        assertThat(pawn.getRepresentation()).isEqualTo(representation);
-    }
-
-    @Test
-    @DisplayName("폰을 생성한 기본색은 흰색으로 생성된다.")
-    public void create_default() {
-        Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE_COLOR);
-        assertThat(pawn.getRepresentation()).isEqualTo(Pawn.WHITE_REPRESENTATION);
+    public void verifyPiece(final Piece piece, final String color, final String representation) {
+        assertThat(piece.getColor()).isEqualTo(color);
+        assertThat(piece.getRepresentation()).isEqualTo(representation);
     }
 
 }
